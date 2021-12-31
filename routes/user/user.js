1faad79529ba7602b3a893701da647217cefdb14
andries sebola <andriesmpontsheng@gmail.com>
2022-01-01 10:41:31 +1100

    cvb

--- routes/user/user.js
+++ routes/user/user.js
@@ -48,7 +48,7 @@ const schema = Joi.object({
 });
 
 
-route.get('/',verify, async (req, res) => {
+route.get('/', async (req, res) => {
   User.find({}, (err, results) => {
     res.send(results);
   });
