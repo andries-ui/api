8acab3cf79bf10da17ee96f64172416b888ffbb7
andries sebola <andriesmpontsheng@gmail.com>
2022-01-26 05:38:38 +1100

    commit

--- module/hotel/room/room.js
+++ module/hotel/room/room.js
@@ -4,29 +4,22 @@ const roomSchema = mongoose.Schema({
 
   type: {
     type: String,
-    require: true,
   },
   price: {
     type: String,
-    require: true,
   },
   status: {
     type: Boolean,
-    required: true,
     default: false
   },
   floor: {
     type: String,
-    required: true,
   },
   roomNumber: {
     type: String,
-    required: true,
-    uniquue:true
   },
   hotelId: {
     type: String,
-    required: true,
   },
   createdAt: {
     type: Date,
